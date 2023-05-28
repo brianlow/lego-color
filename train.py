9db4b44bba0eaf6023980c97fea8288c9d8d8b7e
Brian Low <github@brianlow.com>
2023-05-27 20:09:40 -0600

    Update experiment name

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from pathlib import Path
 # export COMET_API_KEY=<your key if you want to report to comet.com>
 # pip install -r requirements.txt!
 
-experiment_name = "color-03-common-5k-trans-real"
+experiment_name = "lego-color-common-5k-dataset-4-real-baseline"
 dataset_name = "lego-color-common-5k-dataset-4-real-baseline"
 
 comet_ml.init(project_name=experiment_name)
